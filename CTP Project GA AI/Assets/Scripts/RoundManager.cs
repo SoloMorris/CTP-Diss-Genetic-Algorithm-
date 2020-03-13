db10e617949668065cf0e15dd1e2c32809152d11
Sol Morrison <solomon2.morrison@live.uwe.ac.uk>
2020-03-13 02:27:18 +0000

    Little tweaks to make way for GA improvements

--- CTP Project GA AI/Assets/Scripts/RoundManager.cs
+++ CTP Project GA AI/Assets/Scripts/RoundManager.cs
@@ -7,6 +7,9 @@ public class RoundManager : MonoBehaviour
     public static RoundManager instance;
     public int currentRound;
     public bool roundActive = false;
+    int epochs = 5;
+    int roundSet;
+
     public float difficulty;
     public float deathTimer = 3.0f;
     public float waveTimer = 0.1f;
@@ -57,14 +60,21 @@ public class RoundManager : MonoBehaviour
 
         var GAinst = GeneticAlien._instance;
         var alienlist = GAinst.GetAlienList();
-
+        
         difficulty = GAinst.FindDifficulty();
 
         for (int i = 0; i < alienlist.Count; i++)
         {
             alienlist[i].GetComponent<GeneticAlien.Alien>().instance.GetComponent<AlienController>().killed = false;
         }
-        GAinst.ga.CreateNewGeneration();
+            GAinst.ga.CreateNewGeneration();
+        if (roundSet == epochs)
+        {
+            //Save the current ga as a JSON file.
+            roundSet = 0;
+        }
+        else
+            roundSet += 1;
         for (int i = 0; i < GAinst.GetAlienList().Count; i++)
         {
             alienlist[i].GetComponent<GeneticAlien.Alien>().movesUsed = 0;
